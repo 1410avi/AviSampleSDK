34c4132d4db77a3002038675e934deafdcaeb9d5
Avinash <avinash.soni@grayquest.com>
2023-12-26 17:26:29 +0530

    Added package1

--- Package.swift
+++ Package.swift
@@ -23,9 +23,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .target(
-//            name: "GrayQuestiOSSDK",
-//            dependencies: ["Razorpay"]),
+        .target(
+            name: "AviSampleSDK",
+            dependencies: []),
 //        .binaryTarget(name: "Razorpay", path: "Razorpay.xcframework.zip"),
 //        .testTarget(
 //            name: "GrayQuestiOSSDKTests",
